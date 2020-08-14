359e7e37abdfe0101907efb8d088302cb09d20ab
Gustavo Maciel <gu.ga.14@hotmail.com>
2020-08-14 16:11:00 -0300

    fix functional tests

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express'
-import googleBooksRouter from './controllers/googlebooks'
 import booksRouter from './controllers/books'
 
 const routes = Router()
 
-routes.use('/googlebooks', googleBooksRouter)
 routes.use('/books', booksRouter)
 
 export default routes
