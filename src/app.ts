12715fb7e8ad7d998aa05c140e7d3c8aa08ff124
Gustavo Maciel <gu.ga.14@hotmail.com>
2020-08-26 14:04:49 -0300

    express logger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from 'express'
 import routes from './routes'
+import cors from 'cors'
+import expressPino from 'express-pino-logger'
+import logger from './logger'
 
 const app = express()
 app.use(express.json())
+app.use(expressPino({
+    logger
+}))
 app.use(routes)
+app.use(cors({
+    origin: '*'
+}))
 
 export default app
